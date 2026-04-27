940355f7c294ef7f75930be332de2e7aad578134
NguyenTrungDung1 <dungchicken1234@gmail.com>
2026-04-27 22:26:45 +0700

    intentional checkstyle error

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,9 +2,8 @@ package com.example;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello CI PR Review");
+    System.out.println("Hello CI PR Review")
   }
-  int BadVariableName = 10
 
   public static int add(int a, int b) {
     return a + b;
