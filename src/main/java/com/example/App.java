7373d0cd923234928b0c41fd7289e56727859906
NguyenTrungDung1 <dungchicken1234@gmail.com>
2026-04-27 22:41:13 +0700

    Final commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,7 +4,6 @@ public class App {
   public static void main(String[] args) {
     System.out.println("Hello CI PR Review");
   }
-  int hi = "stupid";
 
   public static int add(int a, int b) {
     return a + b;
