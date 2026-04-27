e17be24532590c29583d7bd10cfa3d57ff5ed33d
NguyenTrungDung1 <dungchicken1234@gmail.com>
2026-04-27 22:35:52 +0700

    intentional mistake

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,8 +2,9 @@ package com.example;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello CI PR Review")
+    System.out.println("Hello CI PR Review");
   }
+  int hi = "stupid";
 
   public static int add(int a, int b) {
     return a + b;
